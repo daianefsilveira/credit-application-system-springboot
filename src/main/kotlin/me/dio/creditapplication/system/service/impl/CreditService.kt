8c40ad9406a2c1cf855aa27ff7ff4026520eb548
daianefsilveira <daiane.pguni@gmail.com>
2023-07-08 18:49:27 -0300

    Data da primeira parcela deverá ser no máximo 3 meses após o dia atual

--- src/main/kotlin/me/dio/creditapplication/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/creditapplication/system/service/impl/CreditService.kt
@@ -5,6 +5,7 @@ import me.dio.creditapplication.system.exception.BusinessException
 import me.dio.creditapplication.system.repository.CreditRepository
 import me.dio.creditapplication.system.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -13,6 +14,7 @@ class CreditService(
     private val customerService: CustomerService
 ): ICreditService {
     override fun save(credit: Credit): Credit {
+        this.validDayFirstInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -30,5 +32,10 @@ class CreditService(
         } else {
             throw IllegalArgumentException("Contact admin")
         }
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Invalid Date")
     }
 }
