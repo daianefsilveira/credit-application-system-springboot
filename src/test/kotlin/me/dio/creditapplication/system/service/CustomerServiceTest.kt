d998fbda471a9c21e2893531f6af2ce02e1c046e
daianefsilveira <daiane.pguni@gmail.com>
2023-07-12 15:20:49 -0300

    Teste Unitário: CustomerService - findById e exception

--- src/test/kotlin/me/dio/creditapplication/system/service/CustomerServiceTest.kt
+++ src/test/kotlin/me/dio/creditapplication/system/service/CustomerServiceTest.kt
@@ -7,6 +7,7 @@ import io.mockk.junit5.MockKExtension
 import io.mockk.verify
 import me.dio.creditapplication.system.entity.Address
 import me.dio.creditapplication.system.entity.Customer
+import me.dio.creditapplication.system.exception.BusinessException
 import me.dio.creditapplication.system.repository.CustomerRepository
 import me.dio.creditapplication.system.service.impl.CustomerService
 import org.assertj.core.api.Assertions
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.context.ActiveProfiles
 import java.math.BigDecimal
+import java.util.*
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
@@ -35,6 +37,34 @@ class CustomerServiceTest {
         Assertions.assertThat(actual).isNotNull
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
         verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should find customer by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        //when
+        val actual: Customer = customerService.findById(fakeId)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isExactlyInstanceOf(Customer::class.java)
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should not find customer by invalid id and throw BusinessException`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        every { customerRepository.findById(fakeId) } returns Optional.empty()
+        //when
+        //then
+        Assertions.assertThatExceptionOfType(BusinessException::class.java)
+                .isThrownBy { customerService.findById(fakeId) }
+                .withMessage("Id $fakeId not found")
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
     }
 
     private fun buildCustomer(
